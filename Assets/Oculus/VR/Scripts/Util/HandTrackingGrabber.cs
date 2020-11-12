db9ed78b69e357f0a43c8257ec5fd55fc64f227a
Jonathan Lee <48492574+jonlee96@users.noreply.github.com>
2020-11-12 16:26:26 +0900

    still working on the damn hand grabbing shit

--- Assets/Oculus/VR/Scripts/Util/HandTrackingGrabber.cs
+++ Assets/Oculus/VR/Scripts/Util/HandTrackingGrabber.cs
@@ -17,19 +17,21 @@ public class HandTrackingGrabber : OVRGrabber
         CheckIndexPinch();
 
     }
-
     void CheckIndexPinch()
     {
         var hand = GetComponent<OVRHand>();
         bool isIndexFingerPinching = hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
 
-
-        if(isIndexFingerPinching) {
+        if(isIndexFingerPinching && !m_grabbedObj && m_grabCandidates.Count > 0) {
+            //debugText.text = m_grabbedObj.gameObject.name;
             debugText.text = "True";
             GrabBegin();
-        } else {
+        } 
+        else if(!isIndexFingerPinching && m_grabbedObj) {
+            //debugText.text = m_grabbedObj.gameObject.name;
             debugText.text = "False";
             GrabEnd();
         }
+        
     }
 }
