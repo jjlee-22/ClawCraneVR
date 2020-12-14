266cbdf28be34c81ed3b612e4acb549378075129
uioe32109 <yilin.cai.mcs@gmail.com>
2020-12-14 01:10:55 -0800

    add claw grab movement

--- Assets/GrabDetect.cs
+++ Assets/GrabDetect.cs
@@ -6,10 +6,17 @@ public class GrabDetect : MonoBehaviour
 {
     public TestNodePrimitive grabpoint;
     public GameObject PrizePool;
+    public GameObject leftFinger;
+    public GameObject rightFinger;
+    bool grab_complete;
+    bool is_grabbing;
+    bool is_releasing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        grab_complete = false;
+        is_grabbing = false;
+        is_releasing = false;
     }
 
     // Update is called once per frame
@@ -17,10 +24,10 @@ public class GrabDetect : MonoBehaviour
     {
         Vector3 grab_pos = grabpoint.pos;
         transform.position = grab_pos;
-        if (OVRInput.GetDown(OVRInput.Button.One) == true)
+        if (OVRInput.GetDown(OVRInput.Button.One) == true && is_releasing==false)
         {
-            Debug.Log("Press Grab");
-            foreach(Transform child in PrizePool.transform)
+            is_grabbing = true;
+            /*foreach(Transform child in PrizePool.transform)
             {
                 Vector3 prize_pos = child.position;
                 float dist = (prize_pos - transform.position).magnitude;
@@ -30,6 +37,90 @@ public class GrabDetect : MonoBehaviour
                     child.gameObject.GetComponent<Rigidbody>().isKinematic = true;
                     break;
                 }
+            }*/
+        }
+
+        bool left_complete = false;
+        bool right_complete = false;
+        Vector3 left_rot = leftFinger.transform.localRotation.eulerAngles;
+        Vector3 right_rot = rightFinger.transform.localRotation.eulerAngles;
+        if (is_grabbing == true) // grabbing process
+        {
+            if(left_rot.z > 341 || left_rot.z < 11)
+            {
+                left_rot -= new Vector3(0, 0, 20) * Time.deltaTime;
+                leftFinger.transform.localRotation = Quaternion.Euler(left_rot);
+            }
+            else
+            {
+                left_complete = true;
+            }
+            if(right_rot.z > 341 || right_rot.z < 11)
+            {
+                right_rot -= new Vector3(0, 0, 20) * Time.deltaTime;
+                rightFinger.transform.localRotation = Quaternion.Euler(right_rot);
+            }
+            else
+            {
+                right_complete = true;
+            }
+            if(left_complete == true && right_complete == true) //grabbing complete
+            {
+                Debug.Log("Grab Complete");
+                is_grabbing = false;
+                grab_complete = true;
+            }
+        }
+        else if (is_releasing == true)
+        {
+            if (left_rot.z >= 340 || left_rot.z <= 10)
+            {
+                left_rot += new Vector3(0, 0, 20) * Time.deltaTime;
+                leftFinger.transform.localRotation = Quaternion.Euler(left_rot);
+            }
+            else
+            {
+                left_complete = true;
+            }
+            if (right_rot.z >= 340 || right_rot.z <= 10)
+            {
+                right_rot += new Vector3(0, 0, 20) * Time.deltaTime;
+                rightFinger.transform.localRotation = Quaternion.Euler(right_rot);
+            }
+            else
+            {
+                right_complete = true;
+            }
+            if (left_complete == true && right_complete == true) //releasing complete
+            {
+                is_releasing = false;
+                is_grabbing = false;
+                grab_complete = false;
+            }
+        }
+
+        if (grab_complete == true && is_grabbing == false && is_releasing == false && transform.childCount==0) //After grabbing is complete, check if an object is grabbed
+        {
+            Debug.Log("Check if grabbed");
+            bool has_grab = false;
+            foreach (Transform child in PrizePool.transform)
+            {
+                Vector3 prize_pos = child.position;
+                float dist = (prize_pos - transform.position).magnitude;
+                if (dist <= 0.5f)
+                {
+                    Debug.Log("Grabbed something");
+                    has_grab = true;
+                    child.parent = transform;
+                    child.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                    break;
+                }
+            }
+            if (has_grab == false)
+            {
+                Debug.Log("No grab");
+                is_releasing = true;
+                grab_complete = false;
             }
         }
     }
